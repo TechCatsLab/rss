b63a24d0df4377f203a0f0f1ae4828f5ae3c7f00
wanghuajian620 <154659041@qq.com>
2018-08-07 20:58:37 +0800

    rename main.go and main_test.go

---
+++ date.go
@@ -0,0 +1,46 @@
+/*
+ *  Revision History:
+ *      Initial: 2018/08/07    Wang Huajian
+ */
+
+package rss
+
+import (
+	"time"
+)
+
+//Date type
+type Date string
+
+//Parse (Date function) and returns Time, error
+func (d Date) Parse() (time.Time, error) {
+	t, err := d.ParseWithFormat(wordpressDateFormat)
+	if err != nil {
+		t, err = d.ParseWithFormat(time.RFC822) // RSS 2.0 spec
+	}
+	return t, err
+}
+
+//ParseWithFormat (Date function), takes a string and returns Time, error
+func (d Date) ParseWithFormat(format string) (time.Time, error) {
+	return time.Parse(format, string(d))
+}
+
+//Format (Date function), takes a string and returns string, error
+func (d Date) Format(format string) (string, error) {
+	t, err := d.Parse()
+	if err != nil {
+		return "", err
+	}
+	return t.Format(format), nil
+}
+
+//MustFormat (Date function), take a string and returns string
+func (d Date) MustFormat(format string) string {
+	s, err := d.Format(format)
+	if err != nil {
+		return err.Error()
+	}
+	return s
+}
+
--- rss.go
+++ rss.go
@@ -7,14 +7,7 @@
 package rss
 
 import (
-"encoding/xml"
-"net/http"
-"time"
-"crypto/tls"
-
-"github.com/paulrosania/go-charset/charset"
 _ "github.com/paulrosania/go-charset/data" //initialize only
-	"fmt"
 )
 
 const (
@@ -50,40 +43,5 @@ type Item struct {
 	Author 		string          `xml:"author"`
 	Content     string          `xml:"content"`
 	FullText    string          `xml:"full-text"`
-}
-
-//Date type
-type Date string
-
-//Parse (Date function) and returns Time, error
-func (d Date) Parse() (time.Time, error) {
-	t, err := d.ParseWithFormat(wordpressDateFormat)
-	if err != nil {
-		t, err = d.ParseWithFormat(time.RFC822) // RSS 2.0 spec
-	}
-	return t, err
-}
-
-//ParseWithFormat (Date function), takes a string and returns Time, error
-func (d Date) ParseWithFormat(format string) (time.Time, error) {
-	return time.Parse(format, string(d))
-}
-
-//Format (Date function), takes a string and returns string, error
-func (d Date) Format(format string) (string, error) {
-	t, err := d.Parse()
-	if err != nil {
-		return "", err
-	}
-	return t.Format(format), nil
-}
-
-//MustFormat (Date function), take a string and returns string
-func (d Date) MustFormat(format string) string {
-	s, err := d.Format(format)
-	if err != nil {
-		return err.Error()
-	}
-	return s
 }
 
